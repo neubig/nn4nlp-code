cedf9df2b05ca4424c91edf9043e2554ad9d808a
Graham Neubig <neubig@gmail.com>
2017-09-07 06:41:04 -0400

    Added binary prediction example

---
+++ 04-efficiency/wordemb-skip-binary.py
@@ -0,0 +1,104 @@
+from collections import defaultdict
+import math
+import time
+import random
+import dynet as dy
+import numpy as np
+import pdb
+
+N=2 #length of window on each side (so N=2 gives a total window size of 5, as in t-2 t-1 t t+1 t+2)
+EMB_SIZE = 128 # The size of the embedding
+
+embeddings_location = "embeddings.txt" #the file to write the word embeddings to
+labels_location = "labels.txt" #the file to write the labels to
+
+# We reuse the data reading from the language modeling class
+w2i = defaultdict(lambda: len(w2i))
+
+#word counts for negative sampling
+word_counts = defaultdict(int)
+
+S = w2i["<s>"]
+UNK = w2i["<unk>"]
+def read_dataset(filename):
+  with open(filename, "r") as f:
+    for line in f:
+      line = line.strip().split(" ")
+      for word in line:
+        word_counts[w2i[word]] += 1
+      yield [w2i[x] for x in line]
+
+# Read in the data
+train = list(read_dataset("../data/ptb/train.txt"))
+w2i = defaultdict(lambda: UNK, w2i)
+dev = list(read_dataset("../data/ptb/valid.txt"))
+i2w = {v: k for k, v in w2i.items()}
+nwords = len(w2i)
+nbits = len(np.binary_repr(nwords-1))
+
+with open(labels_location, 'w') as labels_file:
+  for i in range(nwords):
+    labels_file.write(i2w[i] + '\n')
+
+# Start DyNet and define trainer
+model = dy.Model()
+trainer = dy.SimpleSGDTrainer(model, learning_rate=0.1)
+
+# Define the model
+W_w_p = model.add_lookup_parameters((nwords, EMB_SIZE)) # Word weights
+W_c_p = model.add_parameters((nbits, EMB_SIZE)) # Binary prediction weights
+
+# Calculate the loss value for the entire sentence
+def calc_sent_loss(sent):
+  # Create a computation graph
+  dy.renew_cg()
+
+  W_c = dy.parameter(W_c_p)
+  
+  # Get embeddings for the sentence
+  emb = [W_w_p[x] for x in sent]
+
+  # Step through the sentence and calculate binary prediction losses
+  all_losses = [] 
+  for i, my_emb in enumerate(emb):
+    scores = dy.logistic(W_c * my_emb)
+    pos_words = ([sent[x] if x >= 0 else S for x in range(i-N,i)] +
+                 [sent[x] if x < len(sent) else S for x in range(i+1,i+N+1)])
+    word_repr = [[float(y) for y in np.binary_repr(x).zfill(nbits)] for x in pos_words]
+    word_repr = [dy.inputVector(x) for x in word_repr]
+    all_losses.extend([dy.binary_log_loss(scores, x) for x in word_repr])
+  return dy.esum(all_losses)
+
+MAX_LEN = 100
+
+for ITER in range(100):
+  print("started iter %r" % ITER)
+  # Perform training
+  random.shuffle(train)
+  train_words, train_loss = 0, 0.0
+  start = time.time()
+  for sent_id, sent in enumerate(train):
+    my_loss = calc_sent_loss(sent)
+    train_loss += my_loss.value()
+    train_words += len(sent)
+    my_loss.backward()
+    trainer.update()
+    if (sent_id+1) % 5000 == 0:
+      print("--finished %r sentences" % (sent_id+1))
+  print("iter %r: train loss/word=%.4f, ppl=%.4f, time=%.2fs" % (ITER, train_loss/train_words, math.exp(train_loss/train_words), time.time()-start))
+  # Evaluate on dev set
+  dev_words, dev_loss = 0, 0.0
+  start = time.time()
+  for sent_id, sent in enumerate(dev):
+    my_loss = calc_sent_loss(sent)
+    dev_loss += my_loss.value()
+    dev_words += len(sent)
+    trainer.update()
+  print("iter %r: dev loss/word=%.4f, ppl=%.4f, time=%.2fs" % (ITER, dev_loss/dev_words, math.exp(dev_loss/dev_words), time.time()-start))
+
+  print("saving embedding files")
+  with open(embeddings_location, 'w') as embeddings_file:
+    W_w_np = W_w_p.as_array()
+    for i in range(nwords):
+      ith_embedding = '\t'.join(map(str, W_w_np[i]))
+      embeddings_file.write(ith_embedding + '\n')
