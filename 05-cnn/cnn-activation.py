c18372d8466bb12b8c603d4c85f881b0ceacb99c
Danish Pruthi <danish037@gmail.com>
2019-02-04 22:58:02 -0500

    changed cnn-activations to work with python3 (erstwhile python2 compatible)

--- 05-cnn/cnn-activation.py
+++ 05-cnn/cnn-activation.py
@@ -24,7 +24,7 @@ nwords = len(w2i)
 ntags = 5
 
 # Start DyNet and define trainer
-model = dy.Model()
+model = dy.ParameterCollection()
 trainer = dy.AdamTrainer(model)
 
 # Define the model
@@ -40,31 +40,23 @@ b_sm = model.add_parameters((ntags))                      # Softmax bias
 
 def calc_scores(wids):
     dy.renew_cg()
-    W_cnn_express = dy.parameter(W_cnn)
-    b_cnn_express = dy.parameter(b_cnn)
-    W_sm_express = dy.parameter(W_sm)
-    b_sm_express = dy.parameter(b_sm)
     if len(wids) < WIN_SIZE:
         wids += [0] * (WIN_SIZE-len(wids))
 
     cnn_in = dy.concatenate([dy.lookup(W_emb, x) for x in wids], d=1)
-    cnn_out = dy.conv2d_bias(cnn_in, W_cnn_express, b_cnn_express, stride=(1, 1), is_valid=False)
+    cnn_out = dy.conv2d_bias(cnn_in, W_cnn, b_cnn, stride=(1, 1), is_valid=False)
     pool_out = dy.max_dim(cnn_out, d=1)
     pool_out = dy.reshape(pool_out, (FILTER_SIZE,))
     pool_out = dy.rectify(pool_out)
-    return W_sm_express * pool_out + b_sm_express
+    return W_sm * pool_out + b_sm
 
 def calc_predict_and_activations(wids, tag, words):
     dy.renew_cg()
-    W_cnn_express = dy.parameter(W_cnn)
-    b_cnn_express = dy.parameter(b_cnn)
-    W_sm_express = dy.parameter(W_sm)
-    b_sm_express = dy.parameter(b_sm)
     if len(wids) < WIN_SIZE:
         wids += [0] * (WIN_SIZE-len(wids))
 
     cnn_in = dy.concatenate([dy.lookup(W_emb, x) for x in wids], d=1)
-    cnn_out = dy.conv2d_bias(cnn_in, W_cnn_express, b_cnn_express, stride=(1, 1), is_valid=False)
+    cnn_out = dy.conv2d_bias(cnn_in, W_cnn, b_cnn, stride=(1, 1), is_valid=False)
     filters = (dy.reshape(cnn_out, (len(wids), FILTER_SIZE))).npvalue()
     activations = filters.argmax(axis=0)
 
@@ -72,26 +64,26 @@ def calc_predict_and_activations(wids, tag, words):
     pool_out = dy.reshape(pool_out, (FILTER_SIZE,))
     pool_out = dy.rectify(pool_out)
 
-    scores = (W_sm_express * pool_out + b_sm_express).npvalue()
-    print '%d ||| %s' % (tag, ' '.join(words))
+    scores = (W_sm * pool_out + b_sm).npvalue()
+    print ('%d ||| %s' % (tag, ' '.join(words)))
     predict = np.argmax(scores)
-    print display_activations(words, activations)
-    print 'scores=%s, predict: %d' % (scores, predict)
+    print (display_activations(words, activations))
+    print ('scores=%s, predict: %d' % (scores, predict))
     features = pool_out.npvalue()
-    W = W_sm_express.npvalue()
-    bias = b_sm_express.npvalue()
-    print '  bias=%s' % bias
+    W = W_sm.npvalue()
+    bias = b_sm.npvalue()
+    print ('  bias=%s' % bias)
     contributions = W * features
-    print ' very bad (%.4f): %s' % (scores[0], contributions[0])
-    print '      bad (%.4f): %s' % (scores[1], contributions[1])
-    print '  neutral (%.4f): %s' % (scores[2], contributions[2])
-    print '     good (%.4f): %s' % (scores[3], contributions[3])
-    print 'very good (%.4f): %s' % (scores[4], contributions[4])
+    print (' very bad (%.4f): %s' % (scores[0], contributions[0]))
+    print ('      bad (%.4f): %s' % (scores[1], contributions[1]))
+    print ('  neutral (%.4f): %s' % (scores[2], contributions[2]))
+    print ('     good (%.4f): %s' % (scores[3], contributions[3]))
+    print ('very good (%.4f): %s' % (scores[4], contributions[4]))
 
 
 def display_activations(words, activations):
-    pad_begin = (WIN_SIZE - 1) / 2
-    pad_end = WIN_SIZE - 1 - pad_begin
+    pad_begin = int((WIN_SIZE - 1) / 2)
+    pad_end = int(WIN_SIZE - 1 - pad_begin)
     words_padded = ['pad' for i in range(pad_begin)] + words + ['pad' for i in range(pad_end)]
 
     ngrams = []
@@ -129,4 +121,5 @@ for ITER in range(10):
 
 for words, wids, tag in dev:
     calc_predict_and_activations(wids, tag, words)
-    raw_input()
+    # input prompt so that the next example is revealed on a key press
+    input()
